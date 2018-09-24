5c2accc58a49ddae6c9e4ee900bee45f80aebea6
Carlos Garcia <carlosfelgarcia@gmail.com>
2018-09-24 12:09:11 -0700

    Start time activity

---
+++ Modules/TimeManager/TimeActivity.py
@@ -0,0 +1,22 @@
+"""It handles all the logic to calculate the time spending on the process."""
+
+
+class TimeActivity(object):
+    """Class that will handle the logic of the processes."""
+
+    def __init__(self):
+        """Constructor. (place holder for now)."""
+        pass
+
+    def getTimePerProcess(self, session):
+        """Calculate the time per process base on the current session.
+
+        :param session: The session of the current processes.
+        :type session: dict
+        """
+        print('Session ---> ', session)
+
+    def getTimePerProcessWeek(self, session):
+        """TODO."""
+        # TODO
+        pass
